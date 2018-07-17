7052eeaa3b62d648483cd084b572ee885e3012a7
sanjayhumrobo@123 <sanjay.humrobo@gmail.com>
2018-07-17 11:55:45 +0530

    add C# program

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+ 
+namespace ConsoleApp22
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+          //  for (int i = 0; i < args.Length; i++)
+         //   {
+               // Console.WriteLine("argument value are{0}", args[i]);
+                //  Console.WriteLine(args.Length);
+                 Console.Write ("welcom to home");
+                 Console.Write (" " + args[0]);
+                 Console.WriteLine (" " + args[1]);
+                
+          //  }
+            Console.ReadLine();
+          
+        }
+        
+
+    }
+
+}
